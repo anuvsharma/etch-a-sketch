472f9e827629464b59fd4240fe2c5c534902e37b
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-10-30 22:27:32 -0700

    added clear and custom size features added
    this segment added a clear button as well as a input and button for make a custom size of the grid.
    the features are also added meaning the clear button will clear the grid and the custom size of the grid can be set upto 100x100.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const grid_container = document.getElementById("grid_container");
+const clearButton = document.getElementById("clearBtn");
+const sizeInput = document.getElementById("sizeInput");
+const sizeButton = document.getElementById("sizeBtn");
 
 function makeGrid(grid_size){
     grid_container.style.gridTemplateColumns = `repeat( ${grid_size}, 1fr)`
@@ -9,10 +12,31 @@ function makeGrid(grid_size){
         grid_cell.className = 'grid_cell';
         grid_cell.addEventListener('mouseenter', () => {
             grid_cell.style.backgroundColor = 'black';
-        })
+        });
         grid_container.appendChild(grid_cell);
     };
 };
+
+function clearGrid(){
+    grid_container.innerHTML = null;
+    makeGrid(size);
+}
+
+clearButton.addEventListener('click', () => {
+    clearGrid();
+})
+
+sizeButton.addEventListener('click', () => {
+    let inputNumber = sizeInput.value;
+    if (inputNumber > 100){
+        alert('Number Cannot Be Greater Than 100!');
+        sizeInput.value = " ";
+        sizeInput.focus();
+        inputNumber = 16;
+    }
+   clearGrid();
+   makeGrid(inputNumber);
+})
 
 let size = 16;
 makeGrid(size);
