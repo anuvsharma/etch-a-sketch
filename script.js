ef39d12fc287238a9fd7ba5e1fc9f3fd65d2b94a
Anuvrat Sharma <65840212+anuvsharma@users.noreply.github.com>
2023-10-30 20:18:03 -0700

    added the grid
    has added the code that automatically makes the 16x16 etch-a-sketch grid with javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+const grid_container = document.getElementById("grid_container");
+
+function makeGrid(grid_size){
+    grid_container.style.gridTemplateColumns = `repeat( ${grid_size}, 1fr)`
+    grid_container.style.gridTemplateRows = `repeat( ${grid_size}, 1fr)`;
+    for (c = 0; c < (grid_size*grid_size); c++){
+        let grid_cell = document.createElement('div');
+        grid_cell.style.backgroundColor = 'white';
+        grid_cell.className = 'grid_cell';
+        grid_container.appendChild(grid_cell);
+    };
+};
+
+let size = 16;
+makeGrid(size);
